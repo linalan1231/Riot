2b89373f0c6e5d3b7ab47374cd282c1419ed519d
Alan Lin <alan.lin1@baruchmail.cuny.edu>
2024-08-05 15:33:21 -0400

    reshaping data and converting it into a dataframe

--- Code/Testing.py
+++ Code/Testing.py
@@ -1,11 +1,12 @@
 # !pip3 install riotwatcher
 import requests
 import pandas as pd
+import numpy as np
 
 # Setting up Api key and URL to establish connection
 api_key  = "RGAPI-c3a7bab0-2e16-4ff7-92d1-58d667da76fb" # Key expires every 24 hours, please renew at https://developer.riotgames.com
-tag = "NA2"
-league_name ="Better team wins"
+tag = "NA1"
+league_name ="SuSuN PaNdiE"
 url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{league_name}/{tag}"
 
 # Checking the response -> 400 bad, 401 authorized, 403 forbidden, 404 data not found, 405 method not found.
@@ -53,6 +54,10 @@ def get_player_info(game_info, player_name):
 game_history = get_match_history(20)
 print(len(game_history))
 
-player_info = get_player_info(game_history, "Better Team wins")
+player_info = get_player_info(game_history, "SuSuN PaNdiE")
 print(player_info)
 
+player_info = np.array(player_info).reshape(-1,5)
+player_info
+df = pd.DataFrame(player_info, columns =["player_name","kills","deaths","assists","KDA"])
+df
